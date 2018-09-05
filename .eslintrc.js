2b7e95d74a61293826ed77ef3d6f6ce6bd881235
elevensky <346800989@qq.com>
2018-09-05 09:55:22 +0800

    feat lint hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,27 @@
 /**
   http://eslint.cn/docs/user-guide/configuring
+  0|off 关闭、1|warn 开启警告级别、2|error 开启错误级别
 */
-{
+
+module.exports = {
   "env": {
-    "browser": true
+    "es6": true,
+    "browser": true,
+    "node": true
   },
-  "extends": ["airbnb"],
+  "extends": ["airbnb", "prettier"],
+  "plugins": ["prettier"],
   "parserOptions": {
     "ecmaFeatures": {
-        "experimentalObjectRestSpread": true,
-        "jsx": true
+      "experimentalObjectRestSpread": true,
+      "jsx": true
     },
     "sourceType": "module"
   },
   "parser": "babel-eslint",
   "rules": {
-    "class-methods-use-this": 0, // 0|off 关闭、1|warn 开启警告级别、2|error 开启错误级别
+    "prettier/prettier": ["error"],
+    "class-methods-use-this": 0,
     "import/no-named-as-default": 0,
     "react/jsx-filename-extension": [
       "error",
